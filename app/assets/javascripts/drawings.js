d3c5a220a3a77f4f3f1eba91eb1fcd4d14b6d687
github username <github email address>
2015-05-01 09:21:42 -0400

    if paint, collects mouseCoordinates in array on mouseMove

--- app/assets/javascripts/drawings.js
+++ app/assets/javascripts/drawings.js
@@ -1,48 +1,56 @@
 $(function(){
   var canvasDiv = document.getElementById('canvasDiv');
   canvas = document.createElement("canvas");
-  canvas.setAttribute("width", "500px");
-  canvas.setAttribute("height", "300px");
+  canvas.setAttribute("width", "900px");
+  canvas.setAttribute("height", "600px");
   canvas.setAttribute("id", "canvas");
   canvasDiv.appendChild(canvas);
 
   context = canvas.getContext("2d");
 
   var paint;
+  // xMovement = new Array();
+  // yMovement = new Array();
+  mouseCoordinates = new Array();
   $("#canvas").on("mousedown", mouseDown);
 });
 
-function mouseDown(){
+function mouseDown(e){
   paint = true;
+  mouseCoordinates.push([e.clientX, e.clientY])
   $(this).on("mousemove", mouseMove);
 }
 
 function mouseMove(e){
-  $(this).on("mouseup", mouseUp);
+  if(paint){
+    mouseCoordinates.push([e.clientX, e.clientY])
+    $(this).on("mouseup", mouseUp);
 
-  var xPosition = e.clientX;
-  var yPosition = e.clientY;
+  // var xPosition = e.clientX;
+  // var yPosition = e.clientY;
 
-  xMovement = new Array();
-  yMovement = new Array();
-
-  xMovement.push(xPosition);
-  yMovement.push(yPosition);
-  draw();
+  // xMovement = new Array();
+  // yMovement = new Array();
+    // xMovement.push(e.clientX);
+    // yMovement.push(e.clientY);
+    // mouseMove(e);
+    draw(e);
+  }
 }
 
 function mouseUp(e){
-  xMovement.push(e.clientX);
-  yMovement.push(e.clientY);
+  // xMovement.push(e.clientX);
+  // yMovement.push(e.clientY);
   paint = false;
 }
 
-function draw(){
+function draw(e){
   if(paint){
     context.beginPath();
-    context.strokeStyle = "blue";
-    context.moveTo(xMovement[0], yMovement[0])
-    context.lineTo(100, 100)
+    context.strokeStyle = "green";
+    context.lineJoin = "round";
+    context.moveTo(mouseCoordinates[0][0], mouseCoordinates[0][1])
+    context.lineTo(e.clientX, e.clientY)
     context.stroke();
   }
 }
