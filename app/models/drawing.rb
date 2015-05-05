43c9230ae28a56dee8cbbf2e640b42e60a6fc848
github username <github email address>
2015-05-05 06:57:46 -0400

    added html5colorpicker

--- app/models/drawing.rb
+++ app/models/drawing.rb
@@ -1,4 +1,4 @@
 class Drawing < ActiveRecord::Base
   COLORS = ["blue", "yellow", "red", "green", "pink", "sienna", "purple", "black", "white", "orange"]
-  TIPS = [3, 6, 9, 12, 15, 18, 21, 24]
+  TIPS = [1, 3, 6, 9, 12, 15, 18, 21, 24]
 end
