8fb5837c98d8db1d8d09e29114690085c9be4185
jmsardina <jenni.sardina@gmail.com>
2015-07-14 07:04:49 -0400

    Moved JS variables to the top of their scope.

--- app/assets/javascripts/responsive_design/responsive_canvases.js
+++ app/assets/javascripts/responsive_design/responsive_canvases.js
@@ -1,15 +1,15 @@
 $(function(){
 	$(window).on("resize", resizeCanvas);
 	$(window).on("resize", hideModal("#completed-drawing-modal"));
+	var drawingCanvas = document.getElementById("drawing-canvas");
+	var copiedDrawing = document.createElement("img");
 
 	function resizeCanvas(){
 		var currentOpacity = imageHolderContext.globalAlpha;
 		var currentColor = drawingContext.strokeStyle;
 		var currentTip = drawingContext.lineWidth;
 
-		var drawingCanvas = document.getElementById("drawing-canvas");
 		var dataURL = drawingCanvas.toDataURL();
-		var copiedDrawing = document.createElement("img");
 		copiedDrawing.setAttribute("src", dataURL);
 
 		$("canvas").attr( "height", $(".canvas-section").height() );
